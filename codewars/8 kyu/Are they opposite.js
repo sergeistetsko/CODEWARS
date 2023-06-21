cad26a1f60f415820a7c46d9544b7b0e09dcb85d
sergeistetsko <sergeistetsko2021@yandex.by>
2023-06-21 12:48:29 +0300

    Are they opposite

---
+++ codewars/8 kyu/Are they opposite.js
@@ -0,0 +1,9 @@
+const isOpposite = (s1, s2) => {
+    if (s1 === s2 || s1.toLowerCase() !== s2.toLowerCase()) return false
+  
+    for (let i = 0; i < s1.length; i++) {
+      if (s1.charAt(i) === s2.charAt(i)) return false
+    }
+  
+    return true
+  }
